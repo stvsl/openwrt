b7fe486577494bcec9e4d9bf9041d1406c1c9490
stvsl <41932612+stvsl@users.noreply.github.com>
2022-11-16 23:05:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,22 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' ./include/image.mk
-sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
+
+# curl -fsSL  https://raw.githubusercontent.com/siropboy/other/master/patch/poweroff/poweroff.htm > ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_system/poweroff.htm 
+# curl -fsSL  https://raw.githubusercontent.com/siropboy/other/master/patch/poweroff/system.lua > ./feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
+# curl -fsSL  https://raw.githubusercontent.com/firkerword/KPR/main/logo.jpg > .package/luci-app-serverchan/root/usr/bin/serverchan/api/logo.jpg
+curl -fsSL https://raw.githubusercontent.com/coolsnowwolf/luci/master/applications/luci-app-adbyby-plus/root/usr/share/adbyby/ad-update > ./feeds/luci/applications/luci-app-adbyby-plus/root/usr/share/adbyby/ad-update
+curl -fsSL  https://raw.githubusercontent.com/firkerword/KPR/main/cus_config.yaml > ./package/openwrt-mos/luci-app-mosdns/root/etc/mosdns/cus_config.yaml
+# Modify default IP
+# sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
 sed -i 's/bootstrap/argon/g' ./feeds/luci/modules/luci-base/root/etc/config/luci
-rm -rf ./feeds/luci/themes/luci-theme-argon
+rm -rf ./package/diy-ziyong/theme
+# rm -rf ./package/diy-ziyong/adguardhome
+rm -rf ./feeds/packages/net/adguardhome
+rm -rf ./feeds/packages/net/smartdns
+rm -rf ./feeds/packages/net/mosdns
+rm -rf ./feeds/luci/applications/luci-app-passwall
+rm -rf ./feeds/luci/applications/luci-app-ssr-plus
+rm -rf ./feeds/luci/applications/luci-app-openclash
+# rm -rf ./feeds/luci/applications/luci-app-adbyby-plus
